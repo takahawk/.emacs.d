345ffab08be9c359c6d407a151b14cd4f9e4d583
takahawk <takahawkkun@gmail.com>
2017-07-29 12:56:50 +0300

    basic settings added

---
+++ init.el
@@ -0,0 +1,17 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(electric-pair-mode 1)
+(show-paren-mode t)
+(global-linum-mode t)
+
+(custom-set-variables
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-enabled-themes (quote (wheatgrass))))
+(custom-set-faces
+)
