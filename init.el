e31404b0da312ccf2816571dbac425c7b0797676
takahawk <takahawkku@gmail.com>
2017-09-28 19:55:21 +0300

    Web mode added with configuration for Rails templates

--- init.el
+++ init.el
@@ -80,6 +80,16 @@
 
 (add-hook 'shell-mode-hook 'shell-mode-config)
 
+;; Web
+(defun web-mode-config ()
+  (setq web-mode-markup-indent-offset 2))
+
+(use-package web-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-hook 'web-mode-hook 'web-mode-config))
+
 ;; C
 (defun c-mode-config ()
   (setq indent-tabs-mode t)
@@ -131,7 +141,7 @@
  '(minimap-window-location (quote right))
  '(package-selected-packages
    (quote
-    (bash-completion groovy-mode escreen cargo company racer racer-mode company-mode magit yaml-mode kotlin-mode rust-mode use-package))))
+    (minimap bash-completion groovy-mode escreen cargo company racer racer-mode company-mode magit yaml-mode kotlin-mode rust-mode use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
