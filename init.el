cd475aba31c901489dea712daecd24ee63c0d5f9
Dan <takahawkkun@gmail.com>
2018-02-03 20:06:39 +0200

    Kill ring size changed to 1000

--- init.el
+++ init.el
@@ -5,8 +5,6 @@
 	("melpa-stable" . "https://stable.melpa.org/packages/")))
 (package-initialize)
 
-(require 'helm-config)
-
 ;; appearance
 (load-theme 'dakrone t)
 (menu-bar-mode      -1)
@@ -37,13 +35,17 @@
 (setq shift-select-mode       nil)
 
 
+;; other config
+(setq kill-ring-max 1000)
+
+
 ;; customize interface
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (helm dakrone-theme))))
+ '(package-selected-packages (quote (helm auctex dakrone-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
